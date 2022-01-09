e91dc5f76076ee88dac16003be9bc176215bff46
Vishal Bhalerao <vishalbhalerao.bhalerao@gmail.com>
2022-01-09 12:18:09 +0530

    addig insert and get operations to db

--- src/ex4.js
+++ src/ex4.js
@@ -38,6 +38,7 @@ async function main() {
 	var myDB = new sqlite3.Database(DB_PATH);
 	// creating helper functions
 	SQL3 = {
+		// for insert, update, delete row
 		run(...args) {
 			return new Promise(function c(resolve, reject) {
 				myDB.run(...args, function onResult(err) {
@@ -47,22 +48,89 @@ async function main() {
 			});
 		},
 		// converting callback based functions to promises for cleaner interface
+		// to get single row
 		get: util.promisify(myDB.get.bind(myDB)),
+		// to get multiple rows
 		all: util.promisify(myDB.all.bind(myDB)),
+		// to execute commands
 		exec: util.promisify(myDB.exec.bind(myDB)),
 	};
-
+	// SQL mydb.sql has queries to create tables if not created before
 	var initSQL = fs.readFileSync(DB_SQL_PATH, "utf-8");
 	// TODO: initialize the DB structure
+	// asynchronous operation, creating Content and UserData tables
+	await SQL3.exec(initSQL)
 
-
-	var other = args.other;
-	var something = Math.trunc(Math.random() * 1E9);
-
+	var userData = args.other;
+	var content = Math.trunc(Math.random() * 1E9);
+	var userDataID = InsertOrLookUp()
+	if (userDataID) {
+		await insertContent(userDataID)
+	}
 	// ***********
 
 	// TODO: insert values and print all records
-
+	async function InsertOrLookUp(userData) {
+		// lookup to find presence of same data
+		var result = await SQL3.get(
+			`
+			SELECT 
+				id
+			FROM
+				UserData
+			WHERE 
+				Data = ?
+			`,
+			userData
+		)
+		if (result && result.id) {
+			return result.id
+		}
+		// if data not present add data into table
+		else {
+			result = await SQL3.run(
+				`
+				INSERT INTO
+					UserData (data)
+				VALUES
+					(?)
+				`,
+				userData
+			)
+			if (result && result.lastID) {
+				return result.lastID
+			}
+		};
+	}
+	async function insertContent(userDataID) {
+		var result = await SQL3.run(
+			`
+			INSERT INTO
+				Content (userDataID, data)
+			VALUES
+				(?, ?)
+			`,
+			userDataID, content
+		)
+		if (result && result.lastID) {
+			return result.lastID
+		}
+	}
+	async function printALlData() {
+		var result = await SQL3.get(
+			`
+			SELECT 
+				id as index
+				data as UserData
+				data as 
+			FROM
+				UserData
+			WHERE 
+				Data = ?
+			`,
+			userData
+		)
+	}
 	error("Oops!");
 }
 
